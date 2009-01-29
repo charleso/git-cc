bc182316e1b8c69184f5671e8ac56015e403cab1
Charles O'Farrell <charleso@charleso.org>
2009-01-29 13:43:28 +1000

    Added update command for people who don't need or want Clearcase history

---
+++ update.py
@@ -0,0 +1,11 @@
+"""Update the git repository with Clearcase manually, ignoring history"""
+
+from common import *
+import sync, reset
+
+def main(message):
+    cc_exec(['update', '.'])
+    sync.main('*')
+    git_exec(['add', '.'])
+    git_exec(['commit', '-m', message])
+    reset.main('HEAD')
