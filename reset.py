7f074a8394a2b6299c8ef37d7e6ef5e245e58ef1
Charles O'Farrell <charleso@charleso.org>
2009-03-03 16:58:10 +1000

    Made reset not actually do reset --hard

--- reset.py
+++ reset.py
@@ -3,6 +3,5 @@
 from common import *
 
 def main(commit):
-    git_exec(['reset', '--hard', commit])
-    git_exec(['branch', '-f', CC_TAG])
-    tag(CI_TAG)
+    git_exec(['branch', '-f', CC_TAG, commit])
+    tag(CI_TAG, commit)
