aa0c6b0bc8a0d9111a4c75176d05b55e6dd75fa0
Pieter Swinkels <swinkels.pieter@yahoo.com>
2016-08-17 10:52:33 +0200

    Fixes test of git_cc imports
    Under Python 3, this test started to fail as more test started to import
    git_cc modules. It turned out that that was caused by this test, whose
    dynamic import of git_cc modules lead to conflicts.

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,6 +1,6 @@
 import os
-import imp
 import pkgutil
+import subprocess
 import sys
 import unittest
 
@@ -8,7 +8,7 @@ import unittest
 class ImportTestSuite(unittest.TestCase):
 
     def test_import_of_each_module(self):
-        """Import each module of package git_cc.
+        """Test whether the import each module of package git_cc succeeds.
 
         This test was added because the import Python 3 requires you to use the
         relative package import syntax "from . import <module name>" to import
@@ -19,36 +19,23 @@ class ImportTestSuite(unittest.TestCase):
         import git_cc
         package_dir = os.path.dirname(os.path.abspath(git_cc.__file__))
         for _, module_name, _ in pkgutil.iter_modules([package_dir]):
-            self.try_module_import(package_dir, module_name)
+            self.check_module_import(package_dir, module_name)
 
-    def try_module_import(self, package_dir, module_name):
-        """Try to import the given module from the given directory.
+    def check_module_import(self, package_dir, module_name):
+        """Return true if and only if the import of the given module succeeds.
 
         If the import fails, this method throws an exception.
-
-        Note that when the import was successful, this method immediately
-        removes it from the list of loaded modules.
-
         """
 
-        module_path = os.path.join(package_dir, module_name) + ".py"
-
-        # The call to imp.load_source fails when it indirectly imports a
-        # module that was already loaded by a previous call to
-        # imp.load_source under the same name.
+        # To test whether the given module can be imported, we start a new
+        # Python process and let that process import the module. In this way
+        # the current Python process is not affected by these trial imports.
         #
-        # To give an example, say you have the following pseudo-code:
-        #
-        #    imp.load_source("package.module-a", ... )
-        #    imp.load_source("package.module-b", ... )
-        #
-        # and module-b has an import of module a, then the second call
-        # throws an ImportError with the message that it cannot import name
-        # "module-b".
-        #
-        # To avoid this issue, we remove the module from the list of imported
-        # modules once the import has succeeded.
+        # An earlier version of this test used the Python library 'imp' and
+        # function 'load_source' to load the given module in the current Python
+        # process. This lead to conflicts with the actual import of these
+        # modules.
 
         full_module_name = "git_cc." + module_name
-        imp.load_source(full_module_name, module_path)
-        del sys.modules[full_module_name]
+        subprocess.check_call(
+            [sys.executable, "-c", "import " + full_module_name])
